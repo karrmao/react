147ff7112cd657185ac2d5863dd0dd409782114f
karrmao <karrmao@gmail.com>
2022-10-31 14:34:01 +0200

    add

--- less_1_intro/task3/index.js
+++ less_1_intro/task3/index.js
@@ -23,16 +23,14 @@
 /***create with React***/
 const rootElement = document.querySelector('#root');
 
-const element = React.createElement(
-  'div',
-  { className: 'greeting' },
+const element = React.createElement('div', { className: 'greeting' }, [
   React.createElement('div', { className: 'greeting__title' }, 'Hello, world!'),
   React.createElement(
     'div',
     { className: 'greeting__text' },
     `I'm learning React`
-  )
-);
+  ),
+]);
 
 ReactDOM.render(element, rootElement);
 
