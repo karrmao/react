583f6c457cadcd1ca32275e30fe924776dc47450
karrmao <karrmao@gmail.com>
2022-11-01 20:10:54 +0200

    add

--- less_2_jsx/task2/src/index.js
+++ less_2_jsx/task2/src/index.js
@@ -1,60 +1,33 @@
-/* <main class="page">
-  <form class="login-form">
-    <h1 class="form-title">Profile</h1>
-    <div class="form-control">
-      <label class="form-label" for="name">
-        Name
-      </label>
-      <input
-        class="form-input"
-        type="text"
-        id="name"
-        name="name"
-        value="anonymous"
-      />
-    </div>
-    <div class="form-control">
-      <label class="form-label" id="age" for="age">
-        Age
-      </label>
-      <input class="form-input" type="number" value="17" name="age" />
-      <span style="color: red; font-weight: 700">To young</span>
-    </div>
-    <button class="submit-button" type="submit">
-      Submit
-    </button>
-  </form>
-</main>; */
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.css';
+import './styles.css';
 
 const rootElement = document.querySelector('#root');
+const style = { color: 'red', fontWeight: 700 };
 const element = (
-  <main class="page">
-    <form class="login-form">
-      <h1 class="form-title">Profile</h1>
-      <div class="form-control">
-        <label class="form-label" for="name">
+  <main className="page">
+    <form className="login-form">
+      <h1 className="form-title">Profile</h1>
+      <div className="form-control">
+        <label className="form-label" htmlFor="name">
           Name
         </label>
         <input
-          class="form-input"
+          className="form-input"
           type="text"
           id="name"
           name="name"
           value="anonymous"
         />
       </div>
-      <div class="form-control">
-        <label class="form-label" id="age" for="age">
+      <div className="form-control">
+        <label className="form-label" id="age" htmlFor="age">
           Age
         </label>
-        <input class="form-input" type="number" value="17" name="age" />
-        <span style="color: red; font-weight: 700">To young</span>
+        <input className="form-input" type="number" value="17" name="age" />
+        <span style={style}>To young</span>
       </div>
-      <button class="submit-button" type="submit">
+      <button className="submit-button" type="submit">
         Submit
       </button>
     </form>
