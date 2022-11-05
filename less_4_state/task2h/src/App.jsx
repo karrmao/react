3a8e98036d6b0eef41d2e425178a798bea56640d
karrmao <karrmao@gmail.com>
2022-11-05 17:44:56 +0200

    add

--- less_4_state/task2h/src/App.jsx
+++ less_4_state/task2h/src/App.jsx
@@ -4,9 +4,9 @@ import Clock from './Clock';
 const App = () => {
   return (
     <>
-      <Clock offset={-5} location={'New York'} />
-      <Clock offset={2} location={'Kyiv'} />
       <Clock offset={0} location={'London'} />
+      <Clock offset={2} location={'Kyiv'} />
+      <Clock offset={-5} location={'New York'} />
     </>
   );
 };
