86df7dd44097731ca0fcb11f5e445d69c62161fc
karrmao <karrmao@gmail.com>
2022-11-07 22:59:15 +0200

    add

--- less_6_rendering/task3/src/index.jsx
+++ less_6_rendering/task3/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Mailbox from './Mailbox';
+import Page from './Page';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<Mailbox unreadMessages={['s']} />, rootElement);
+ReactDOM.render(<Page />, rootElement);
