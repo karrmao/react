3258769c88d911e7dbd78be2e3d2f1765cc06329
karrmao <karrmao@gmail.com>
2022-11-07 13:44:46 +0200

    add

--- less_5_event/task5h/src/Toggler.jsx
+++ less_5_event/task5h/src/Toggler.jsx
@@ -25,26 +25,3 @@ class Toggler extends Component {
 }
 
 export default Toggler;
-
-// class Toggler extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       status: 'Off',
-//     };
-//   }
-
-//   changeStatus = () => {
-//     this.setState({
-//       status: this.state.status === 'Off' ? 'On' : 'Off',
-//     });
-//   };
-
-//   render() {
-//     return (
-//       <button onClick={this.changeStatus} className="toggler">
-//         {this.state.status}
-//       </button>
-//     );
-//   }
-// }
---
+++ less_5_event/task6h/src/ColorPicker.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class ColorPicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      textColorName: '',
+    };
+  }
+
+  setColorName = color => {
+    this.setState({
+      textColorName: color,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="picker__title">{this.state.textColorName}</div>
+        <div>
+          <button
+            className="picker__button picker__button_coral"
+            onMouseEnter={() => this.setColorName('Coral')}
+            onMouseLeave={() => this.setColorName('')}
+          ></button>
+          <button
+            className="picker__button picker__button_aqua"
+            onMouseEnter={() => this.setColorName('Aqua')}
+            onMouseLeave={() => this.setColorName('')}
+          ></button>
+          <button
+            className="picker__button picker__button_bisque"
+            onMouseEnter={() => this.setColorName('Bisque')}
+            onMouseLeave={() => this.setColorName('')}
+          ></button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ColorPicker;
