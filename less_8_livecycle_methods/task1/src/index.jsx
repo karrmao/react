3ddae2d197200533170a73da92728b3f747054c9
karrmao <karrmao@gmail.com>
2022-11-10 15:34:09 +0200

    add

---
+++ less_8_livecycle_methods/task1/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+//import Demo from './Demo';
+import Life from './Life';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<Life />, rootElement);
