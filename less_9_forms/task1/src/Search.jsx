9f3125cdd84dedbef2931790583006c491a3926c
karrmao <karrmao@gmail.com>
2022-11-11 20:49:58 +0200

    add

---
+++ less_9_forms/task1/src/Search.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  state = { value: '' };
+
+  handleChange = event => {
+    console.log(event.target.value);
+    //set value to state ↓↓↓
+    this.setState({ value: event.target.value });
+  };
+  //search btn↓↓↓
+  search = event => {
+    //cancel the default behavior↓↓↓
+    event.preventDefault();
+    console.log(this.state.value);
+  };
+
+  render() {
+    return (
+      <form className="search" onSubmit={this.search}>
+        <input
+          type="text"
+          className="search__input"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <button className="search__button" type="submit">
+          Search
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Search;
