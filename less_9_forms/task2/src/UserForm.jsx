649d532c6bbe48e1fccc6b6ebb831ab6095acd9d
karrmao <karrmao@gmail.com>
2022-11-12 15:25:44 +0200

    add

--- less_9_forms/task2/src/UserForm.jsx
+++ less_9_forms/task2/src/UserForm.jsx
@@ -25,7 +25,13 @@ class UserForm extends Component {
 
   render() {
     return (
-      <form className="login-form" onSubmit={() => props.onSubmit(this.state)}>
+      <form
+        className="login-form"
+        onSubmit={e => {
+          e.preventDefault();
+          this.props.onSubmit(this.state);
+        }}
+      >
         <h1 className="form-title">Profile</h1>
         <div className="form-control">
           <label className="form-label" htmlFor="name">
