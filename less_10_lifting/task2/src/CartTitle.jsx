5bcddec087fe4461290c634374c65ad5bc8455ab
karrmao <karrmao@gmail.com>
2022-11-16 15:08:21 +0200

    add

--- less_10_lifting/task2/src/CartTitle.jsx
+++ less_10_lifting/task2/src/CartTitle.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { render } from 'sass';
 
 const CartTitle = ({ userName, count }) => {
-  return <div className="cart-title">{`${userName}, you added ${count} product`}</div>;
+  return <div className="cart-title">{`${userName}, you added ${count} items`}</div>;
 };
 export default CartTitle;
