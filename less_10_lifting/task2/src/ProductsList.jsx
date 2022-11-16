c4f219ae1046d69d6fea911071e1a469f978f3f9
karrmao <karrmao@gmail.com>
2022-11-16 15:12:57 +0200

    add

--- less_10_lifting/task2/src/ProductsList.jsx
+++ less_10_lifting/task2/src/ProductsList.jsx
@@ -14,6 +14,7 @@ class ProductsList extends Component {
             </li>
           ))}
         </ul>
+        <div className="products__total">{`Total: $${total}`}</div>
       </div>
     );
   }
