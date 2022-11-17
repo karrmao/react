9e258bac41fe4b115e86471ec44e1dc30e281b7f
karrmao <karrmao@gmail.com>
2022-11-17 13:14:51 +0200

    add

--- less_10_lifting/task4h/src/FilterableProductTable.jsx
+++ less_10_lifting/task4h/src/FilterableProductTable.jsx
@@ -2,5 +2,14 @@ import React, { Component } from 'react';
 import ProductTable from './ProductTable';
 import SearchBar from './SearchBar';
 
-class FilterableProductTable extends Component {}
+class FilterableProductTable extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <ProductTable products={this.props.products} />
+      </div>
+    );
+  }
+}
 export default FilterableProductTable;
