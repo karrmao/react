968a3f3dda2911a239c3bc6c7d9ea54ed95ef9e6
karrmao <karrmao@gmail.com>
2022-11-17 13:04:13 +0200

    add

---
+++ less_10_lifting/task4h/src/FilterableProductTable.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import ProductTable from './ProductTable';
+import SearchBar from './SearchBar';
+
+class FilterableProductTable extends Component {}
+export default FilterableProductTable;
