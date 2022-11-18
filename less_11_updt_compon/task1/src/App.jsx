b824f73449e4a1e108b95b13a72c1f05bb4316fc
karrmao <karrmao@gmail.com>
2022-11-18 22:58:14 +0200

    a

--- less_11_updt_compon/task1/src/App.jsx
+++ less_11_updt_compon/task1/src/App.jsx
@@ -1,37 +1,35 @@
 import React, { Component } from 'react';
-import UserProfile from './UserProfile';
-import UserMenu from './UserMenu';
+import Numbers from './Numbers';
+import EvenNumbers from './EvenNumbers';
+import OddNumbers from './OddNumbers';
 
 class App extends Component {
   state = {
-    userData: null,
+    number: 0,
   };
 
-  componentDidMount() {
-    this.fetchUserData(this.props.userId);
-  }
-
-  fetchUserData = userId => {
-    const userUrl = `https://api.github.com/users/${userId}`;
-    fetch(userUrl)
-      .then(response => response.json())
-      .then(userData => {
-        this.setState({
-          userData,
-        });
-      });
-  };
+  //   componentDidMount() {
+  //     this.intervalId = setInterval(() => {
+  //       this.setState({
+  //         number: this.state.number + 1,
+  //       });
+  //     }, 1000);
+  //   }
+  //   //reset value
+  //   componentWillUnmount() {
+  //     clearInterval(this.intervalId);
+  //   }
 
   render() {
     return (
-      <div className="page">
-        <header className="header">
-          <UserMenu userData={this.state.userData} />
-        </header>
-        <UserProfile userData={this.state.userData} />
+      <div className="app">
+        {/* <Numbers title="All numbers" number={this.state.number} />
+        <EvenNumbers title="Event numbers" number={this.state.number} />
+        <OddNumbers title="Odd numbers" number={this.state.number} /> */}
+        <Numbers number={17} />
       </div>
     );
   }
 }
-
 export default App;
+// title = 'All numbers';
