d2fa4185ee51728c14f5b233f2f9fcf7d4abb368
karrmao <karrmao@gmail.com>
2022-11-22 14:46:19 +0200

    add

---
+++ less_11_updt_compon/task3h/src/App.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Dialog from './Dialog';
+
+class App extends Component {
+  state = {
+    isOpen: false,
+  };
+
+  hideDialog = () => {
+    this.setState({
+      isOpen: false,
+    });
+  };
+
+  showDialog = () => {
+    this.setState({
+      isOpen: true,
+    });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <button onClick={this.showDialog} className="btn">
+          Show
+        </button>
+        <Dialog isOpen={this.state.isOpen} onClose={this.hideDialog} title="some title">
+          <p>Some text</p>
+        </Dialog>
+      </div>
+    );
+  }
+}
+export default App;
