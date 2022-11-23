0189f3b98352141d722dde79835287d05a3ba4f7
karrmao <karrmao@gmail.com>
2022-11-23 14:28:05 +0200

    add

--- less_12_todo_list/task_todo_list_part3/src/CreateTaskInput.jsx
+++ less_12_todo_list/task_todo_list_part3/src/CreateTaskInput.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class CreateTaskInput extends Component {
   state = {
@@ -32,4 +33,5 @@ class CreateTaskInput extends Component {
     );
   }
 }
+
 export default CreateTaskInput;
