515b9429e4233309398b5c1fd967f499de238faf
karrmao <karrmao@gmail.com>
2022-11-24 19:25:15 +0200

    add

---
+++ less_13_router/task1/src/Products.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Products = () => {
+  return (
+    <div className="page__content">
+      <h1>Products</h1>
+      <p>We will add some products...</p>
+    </div>
+  );
+};
+export default Products;
