5cdc5c38869930771c44d2184e0ffd8c07dc50a3
karrmao <karrmao@gmail.com>
2022-11-24 20:31:02 +0200

    add

--- less_14_hooks/task1/src/index.jsx
+++ less_14_hooks/task1/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+import Counter from './Counter';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Counter />, rootElement);
