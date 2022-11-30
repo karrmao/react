a3c83540a1f11a814dfec654008aebda54f43b56
karrmao <karrmao@gmail.com>
2022-11-30 11:25:31 +0200

    add

--- less_14_hooks/task3/src/Dimensions.jsx
+++ less_14_hooks/task3/src/Dimensions.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+//import { useParams } from 'react-router-dom';
 
 const Dimensions = () => {
   const [demensions, setDemensions] = useState({ width: null, height: null });
