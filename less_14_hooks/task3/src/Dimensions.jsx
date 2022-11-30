4d0a523a344c0b7c7e8aeda9423455e7fcd0d6b4
karrmao <karrmao@gmail.com>
2022-11-30 11:30:05 +0200

    add

--- less_14_hooks/task3/src/Dimensions.jsx
+++ less_14_hooks/task3/src/Dimensions.jsx
@@ -1,26 +1,25 @@
 import React, { useState, useEffect } from 'react';
-//import { useParams } from 'react-router-dom';
 
 const Dimensions = () => {
-  const [demensions, setDemensions] = useState({ width: null, height: null });
+  const [dimensions, setDimensions] = useState({ width: null, height: null });
 
   useEffect(() => {
     const { innerHeight, innerWidth } = window;
-    setDemensions({ width: innerWidth, height: innerHeight });
+    setDimensions({ width: innerWidth, height: innerHeight });
 
     const handleResize = e => {
       const { innerHeight, innerWidth } = e.target;
-      setDemensions({ width: innerWidth, height: innerHeight });
+      setDimensions({ width: innerWidth, height: innerHeight });
     };
     window.addEventListener('resize', handleResize);
 
     return () => {
-      debugger;
+      //debugger;
       window.removeEventListener('resize', handleResize);
     };
   }, []);
 
-  const { width, height } = demensions;
+  const { width, height } = dimensions;
 
   return (
     <div className="dimensions">
