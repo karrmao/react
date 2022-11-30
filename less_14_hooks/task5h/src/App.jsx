0367a836e39c956fa6a5afa561dbf23022521e59
karrmao <karrmao@gmail.com>
2022-11-30 13:46:56 +0200

    add

--- less_14_hooks/task5h/src/App.jsx
+++ less_14_hooks/task5h/src/App.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
-import ConnectionStatus from './ConnectionStatus';
+import React, { useState } from 'react';
+import Clock from './Clock.jsx';
 
 const App = () => {
-  return <ConnectionStatus />;
+  const [show, setShow] = useState(true);
+
+  const toggle = () => {
+    setShow(!show);
+  };
+
+  return (
+    <>
+      <button className="button" onClick={toggle}>
+        Show / Hide
+      </button>
+      {show && (
+        <>
+          <Clock location="London" offset={0} />
+          <Clock location="Kyiv" offset={2} />
+          <Clock location="New York" offset={-5} />
+        </>
+      )}
+    </>
+  );
 };
 
 export default App;
