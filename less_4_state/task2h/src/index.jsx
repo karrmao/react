4ad9d6a91a64019a4d924f88a6bb657b711f9761
karrmao <karrmao@gmail.com>
2022-12-02 13:09:42 +0200

    a

--- less_4_state/task2h/src/index.jsx
+++ less_4_state/task2h/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import './clock.scss';
 import App from './App.jsx';
 
 const rootElement = document.querySelector('#root');
