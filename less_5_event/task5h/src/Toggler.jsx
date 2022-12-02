ff8e3b02b9abaceb35a363455dca999d89784aa2
karrmao <karrmao@gmail.com>
2022-12-02 19:30:41 +0200

    add

--- less_5_event/task5h/src/Toggler.jsx
+++ less_5_event/task5h/src/Toggler.jsx
@@ -1,27 +1,54 @@
 import React, { Component } from 'react';
 
-class Toggler extends Component {
+// class Toggler extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       statusToggler: 'On',
+//     };
+//   }
+
+//   handleClick = () => {
+//     this.setState({
+//       statusToggler: this.state.statusToggler === 'Off' ? 'On' : 'Off',
+//     });
+//     console.log(this.setState.statusToggler);
+//   };
+
+//   render() {
+//     return (
+//       <div className="toggler" onClick={this.handleClick}>
+//         {this.state.statusToggler}
+//       </div>
+//     );
+//   }
+// }
+
+// export default Toggler;
+
+//option 2
+class Toggler extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { isToggleOn: true };
 
-    this.state = {
-      statusToggler: 'Off',
-    };
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick = () => {
-    this.setState({
-      statusToggler: this.state.statusToggler === 'Off' ? 'On' : 'Off',
-    });
-  };
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn,
+    }));
+  }
 
   render() {
     return (
       <div className="toggler" onClick={this.handleClick}>
-        {this.state.statusToggler}
+        {this.state.isToggleOn ? 'On' : 'Off'}
       </div>
     );
   }
 }
-
 export default Toggler;
