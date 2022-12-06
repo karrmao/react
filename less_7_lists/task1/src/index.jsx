80203429d07df2cdfced9ca35a3f930ba56ac972
karrmao <karrmao@gmail.com>
2022-12-06 22:39:42 +0200

    add

--- less_7_lists/task1/src/index.jsx
+++ less_7_lists/task1/src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import NumbersList from './NumbersList';
+
 const rootElement = document.querySelector('#root');
 
 const numbers = [1, 2, 3, 4, 5];
