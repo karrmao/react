f439fe98231f18ab1905a7b3875d8dd48b6fd56d
karrmao <karrmao@gmail.com>
2022-12-06 21:40:55 +0200

    add

--- less_6_rendering/task4h/src/Status.jsx
+++ less_6_rendering/task4h/src/Status.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Offline from './Offline';
 import Online from './Online';
-
-const Status = props => {
-  return props.isOnline ? <Online /> : <Offline />;
+// const Status = props => {
+//   return props.isOnline ? <Online /> : <Offline />;
+// };
+// export default Status;
+const Status = ({ isOnline }) => {
+  return isOnline ? <Online /> : <Offline />;
 };
 export default Status;
