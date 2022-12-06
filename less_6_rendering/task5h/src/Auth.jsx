cad4044114bc5c0bb73c0b5ec93c2f348dacd30a
karrmao <karrmao@gmail.com>
2022-12-06 22:17:45 +0200

    add

--- less_6_rendering/task5h/src/Auth.jsx
+++ less_6_rendering/task5h/src/Auth.jsx
@@ -3,6 +3,48 @@ import Spinner from './Spinner';
 import Login from './Login';
 import Logout from './Logout';
 
+// class Auth extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       isLoggedIn: false,
+//       isSpinnerTime: false,
+//     };
+//   }
+
+//   loginHendler = () => {
+//     this.setState({
+//       isSpinnerTime: true,
+//     });
+
+//     setTimeout(() => {
+//       this.setState({
+//         isSpinnerTime: false,
+//         isLoggedIn: true,
+//       });
+//     }, 500);
+//   };
+
+//   logoutHendler = () => {
+//     this.setState({
+//       isLoggedIn: false,
+//     });
+//   };
+
+//   render() {
+//     if (this.state.isSpinnerTime) {
+//       return <Spinner size="50px" />;
+//     }
+//     return this.state.isLoggedIn ? (
+//       <Logout onLogout={this.logoutHendler} />
+//     ) : (
+//       <Login onLogin={this.loginHendler} />
+//     );
+//   }
+// }
+// export default Auth;
+
 class Auth extends Component {
   constructor(props) {
     super(props);
